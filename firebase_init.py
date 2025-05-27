7f57c3680a47dfd303a712bef0f4df0e53cbcc21
subinsss <jun443200@naver.com>
2025-05-27 10:44:03 +0000

    환경변수 누락방지

--- firebase_init.py
+++ firebase_init.py
@@ -1,13 +1,17 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 import os
 import json
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 firebase_key_json = os.environ.get("FIREBASE_KEY")
 
-cred_dict = json.loads(firebase_key_json)
+if not firebase_key_json:
+    raise RuntimeError("FIREBASE_KEY 환경변수가 설정되지 않았습니다.")
 
+cred_dict = json.loads(firebase_key_json)
 cred = credentials.Certificate(cred_dict)
-firebase_admin.initialize_app(cred)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
