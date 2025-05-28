a05e925f98150c96aea520e08a008b9d3735f836
subinsss <117307782+subinsss@users.noreply.github.com>
2025-05-28 12:12:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,28 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "✅ Render Flask 서버 살아있음"
+    return "ㅎㅇ"
 
 @app.route("/firebase-data", methods=["POST"])
 def receive_data():
     data = request.get_json()
-    print("🔥 받은 데이터:", data)
-    db.collection("tasks").add(data)
+    print("받은 데이터:", data)
+
+    db.collection("todos").add(data)
+
+    if "title" in data:
+        db.collection("esp_titles").add({"title": data["title"]})
+
     return jsonify({"status": "success", "message": "데이터 저장 완료"}), 200
 
 @app.route("/firebase-data", methods=["GET"])
 def get_data():
-    docs = db.collection("tasks").stream()
+    docs = db.collection("todos").stream()
     data = [doc.to_dict() for doc in docs]
     return jsonify(data), 200
 
-
-@app.route("/esp-task", methods=["GET"])
-def get_latest_task():
-    docs = db.collection("tasks").order_by("createdAt", direction=firestore.Query.DESCENDING).limit(1).stream()
-    latest = [doc.to_dict() for doc in docs]
-    return jsonify(latest), 200
-
+@app.route("/esp-titles", methods=["GET"])
+def get_titles():
+    docs = db.collection("esp_titles").stream()
+    titles = [doc.to_dict().get("title", "") for doc in docs]
+    return jsonify(titles), 200
