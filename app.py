5d0a4c8694f3a633ab0a2fb4b4d481a9976fb8ce
subinsss <117307782+subinsss@users.noreply.github.com>
2025-05-28 14:05:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,24 +10,6 @@ CORS(app)
 def home():
     return "ㅎㅇ"
 
-@app.route("/firebase-data", methods=["POST"])
-def receive_data():
-    data = request.get_json()
-    print("받은 데이터:", data)
-
-    db.collection("todos").add(data)
-
-    if "title" in data:
-        db.collection("esp_titles").add({"title": data["title"]})
-
-    return jsonify({"status": "success", "message": "데이터 저장 완료"}), 200
-
-@app.route("/firebase-data", methods=["GET"])
-def get_data():
-    docs = db.collection("todos").stream()
-    data = [doc.to_dict() for doc in docs]
-    return jsonify(data), 200
-
 @app.route("/esp-titles", methods=["GET"])
 def get_titles():
     docs = db.collection("todos").stream()
